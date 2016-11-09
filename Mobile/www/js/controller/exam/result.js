20186268c856eb45b50289d2109ceab507ac415f
Jireh June Nimes <jjnimes@gmail.com>
2016-11-10 07:36:30 +0800

    Result list

--- Mobile/www/js/controller/exam/result.js
+++ Mobile/www/js/controller/exam/result.js
@@ -6,18 +6,52 @@ angular.module('topper.resultExamCtrl', [])
 		viewData.enableBack = true;
 	}); 
 
-	console.log($stateParams);
+	// console.log($stateParams);
 
 	$scope.questions = $stateParams.questions;
 
 	$scope.answers = $stateParams.answers;
 
-	console.log($scope.questions);
-	console.log($scope.answers);
+	$scope.results = [];
+
+	// console.log($scope.questions);
+	// console.log($scope.answers);
 
 	$scope.goBack = function() {
 		$ionicHistory.goBack();
 	}
+
+	function getResult() {
+		if ($stateParams.questions) {
+			$stateParams.questions.forEach(function(item, index) {
+				var oData = {
+					question_id: item.id,
+					question_text: item.text,
+					question_rationale: item.rationale,
+					question_answer: getObjectByValue(item.selection, 'isAnswer', 1),
+					answer: $stateParams.answers[index].choice,
+					is_answer: $stateParams.answers[index].isAnswer
+				};
+				$scope.results[index] = oData;
+			});
+		}
+	}
+
+	function getObjectByValue(array, key, value) {
+		var _answer = undefined;
+		array.some(function(item, index) {
+			if (item[key] === value) {
+				_answer = array[index].choice;
+				return true;
+			}
+		});
+		return _answer;
+	}
+
+	getResult();
+
+	console.log('RESULT');
+	console.log($scope.results);
 
 });
 
