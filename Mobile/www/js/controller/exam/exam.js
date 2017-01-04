583bb509b475c84d001501d660a169d3d955b4a0
Jireh Nimes <jjnimes@gmail.com>
2017-01-05 07:26:39 +0800

    Updated

--- Mobile/www/js/controller/exam/exam.js
+++ Mobile/www/js/controller/exam/exam.js
@@ -26,9 +26,53 @@ angular.module('topper.examCtrl', [])
 		console.log($scope._sType);
 	});
 
+	$scope.$on('$ionicView.beforeLeave', function (e) {
+	});
+
 	$scope.checkType = function(sType) {
 		return $scope._sType === sType;
 	}
 
+	$scope.selectAnswer = function(data) {
+		$scope.answers[$scope.number] = data;
+	}
+
+	$scope.checkAnswer = function(data) {
+		return {
+			'button-light' : $scope.answers[$scope.number] !== data,
+			'button-assertive' : $scope.answers[$scope.number] === data
+		}
+	}
+
+	$scope.goPrev = function() {
+		if ($scope.number !== 0) {
+			--$scope.number;
+		}
+	}
+
+	$scope.goNext = function() {
+		if ($scope.number === $scope.questions.length - 1) {
+			Popover.confirmEndOfExam().then(
+				function(success) {
+					if (success === true) {
+						if (Object.keys($scope.answers).length !== $scope.questions.length) {
+							alert('There are unanswered questions!');
+						} else {
+							var _oData = {
+								questions: $scope.questions,
+								answers: $scope.answers
+							};
+
+							$rootScope.$emit('ExamAnswers', _oData);
+
+							$state.go('menu.exam_score');
+						}
+					}
+				}
+			);
+		} else {
+			++$scope.number;
+		}
+	}
 });
 
